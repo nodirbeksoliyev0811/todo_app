ea07ed4e79cc4e49ab5cabbc100c82a1bf8270b7
ParaDox-PDT <doniyorjorabekov@gmail.com>
2023-06-24 01:09:35 +0500

    add new screens, add models, update appRouts

---
+++ lib/ui/tab_boxs/tab_box_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TabBoxScreen extends StatefulWidget {
+  const TabBoxScreen({super.key});
+
+  @override
+  State<TabBoxScreen> createState() => _TabBoxScreenState();
+}
+
+class _TabBoxScreenState extends State<TabBoxScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
