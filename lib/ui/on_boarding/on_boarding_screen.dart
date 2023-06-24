3d4d1f633d46f49d10c0439efae4f087b88ea08d
Jamshid Jo'rayev <jorayevjamshid333@gmail.com>
2023-06-24 18:46:53 +0500

    splash screen and on boarding screen create

--- lib/ui/on_boarding/on_boarding_screen.dart
+++ lib/ui/on_boarding/on_boarding_screen.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../../utils/colors.dart';
+import '../../utils/images.dart';
+import '../../utils/svges.dart';
 
 class OnBoardingScreen extends StatefulWidget {
   const OnBoardingScreen({super.key});
@@ -8,8 +14,86 @@ class OnBoardingScreen extends StatefulWidget {
 }
 
 class _OnBoardingScreenState extends State<OnBoardingScreen> {
+  int pageIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: AppColors.C_121212,
+      appBar: AppBar(
+        backgroundColor: AppColors.C_121212,
+        elevation: 0,
+        title: Text("SKIP",
+            style: TextStyle(
+                fontFamily: "Lato", fontSize: 16.sp, color: Colors.grey)),
+      ),
+      body: Column(
+        children: [
+          SizedBox(height: 2.h),
+          Expanded(
+            child: PageView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  Column(
+                    children: [
+                      Image.asset(AppImages.onBoarding1,
+                          height: 277.78.sp, width: 213.w),
+                      SizedBox(height: 51.22.h),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SvgPicture.asset(AppIcons.rec),
+                          SizedBox(width: 2.w),
+                          SvgPicture.asset(AppIcons.recPassive),
+                          SizedBox(width: 2.w),
+                          SvgPicture.asset(AppIcons.recPassive),
+                        ],
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Image.asset(AppImages.onBoarding1,
+                          height: 277.78.sp, width: 213.w),
+                      SizedBox(height: 51.22.h),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SvgPicture.asset(AppIcons.recPassive),
+                          SizedBox(width: 2.w),
+                          SvgPicture.asset(AppIcons.rec),
+                          SizedBox(width: 2.w),
+                          SvgPicture.asset(AppIcons.recPassive),
+                        ],
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Image.asset(AppImages.onBoarding1,
+                          height: 277.78.sp, width: 213.w),
+                      SizedBox(height: 51.22.h),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SvgPicture.asset(AppIcons.recPassive),
+                          SizedBox(width: 2.w),
+                          SvgPicture.asset(AppIcons.recPassive),
+                          SizedBox(width: 2.w),
+                          SvgPicture.asset(AppIcons.rec),
+                        ],
+                      ),
+                    ],
+                  ),]
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/ui/splash/splash_screen.dart
+++ lib/ui/splash/splash_screen.dart
@@ -1,43 +1,44 @@
 import 'package:default_project/ui/app_routes.dart';
+import 'package:default_project/utils/svges.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
-class SplashScreen extends StatefulWidget {
-  const SplashScreen({super.key});
+import '../../utils/colors.dart';
 
-  @override
-  State<SplashScreen> createState() => _SplashScreenState();
-}
-
-class _SplashScreenState extends State<SplashScreen> {
-
-  Future<void> _init() async {
-    await Future.delayed(
-      const Duration(seconds: 2),
-      () {
-        Navigator.pushReplacementNamed(context, RouteNames.loginScreen);
-      },
-    );
-  }
-
-  @override
-  void initState() {
-    _init();
-    super.initState();
-  }
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    _navigateToWelcomeScreen(context);
     return Scaffold(
-      body: Center(
-        child: Text(
-          "Splash Screen",
-          style: TextStyle(
-            fontSize: 30.sp,
-            fontWeight: FontWeight.w700,
-          ),
-        ),
+      backgroundColor: AppColors.C_121212,
+      appBar: AppBar(
+        backgroundColor: AppColors.C_121212,
+        toolbarHeight: 0.h,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Column(
+              children: [
+                SvgPicture.asset(AppIcons.logo,height: 113.h,width: 113.w,),
+                SizedBox(height: 19.h,),
+                Text("UpTodo",style: TextStyle(fontWeight: FontWeight.w700,fontSize: 40.sp,fontFamily: "Lato",color: AppColors.white),),
+
+              ],
+            ),
+          )
+
+        ],
       ),
     );
+  }
+  void _navigateToWelcomeScreen(BuildContext context) async{
+    Future.delayed(Duration(seconds: 3),(){
+      Navigator.pushReplacementNamed(context, RouteNames.onBoardingScreen);
+    });
   }
 }
