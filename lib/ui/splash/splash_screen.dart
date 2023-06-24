73fe15f31d2371fb1c6c2770b5d2eeee8f474444
Jamshid Jo'rayev <jorayevjamshid333@gmail.com>
2023-06-24 17:46:37 +0500

    splash screen half

--- lib/ui/splash/splash_screen.dart
+++ lib/ui/splash/splash_screen.dart
@@ -1,6 +1,10 @@
-import 'package:default_project/ui/app_routes.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../../utils/colors.dart';
+import '../../utils/images.dart';
+import '../../utils/svges.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -10,33 +14,41 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
-
-  Future<void> _init() async {
-    await Future.delayed(
-      const Duration(seconds: 2),
-      () {
-        Navigator.pushReplacementNamed(context, RouteNames.welcomeScreen);
-      },
-    );
-  }
-
-  @override
-  void initState() {
-    _init();
-    super.initState();
-  }
+  int pageIndex=0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(
-          "Splash Screen",
-          style: TextStyle(
-            fontSize: 30.sp,
-            fontWeight: FontWeight.w700,
-          ),
-        ),
+      backgroundColor: AppColors.C_121212,
+      appBar: AppBar(
+        backgroundColor: AppColors.C_121212,
+        elevation: 0,
+        title: Text("SKIP",
+            style: TextStyle(
+                fontFamily: "Lato", fontSize: 16.sp, color: Colors.grey)),
+      ),
+      body: Column(
+        children: [
+          SizedBox(height: 2.h),
+          Center(
+              child: Column(
+            children: [
+              Image.asset(AppImages.onBoarding1,
+              height: 277.78.sp, width: 213.w),
+              SizedBox(height: 51.22.h),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SvgPicture.asset(AppIcons.rec),
+                  SizedBox(width: 2.w),
+                  SvgPicture.asset(AppIcons.recPassive),
+                  SizedBox(width: 2.w),
+                  SvgPicture.asset(AppIcons.recPassive),
+                ],
+              ),
+            ],
+          )),
+        ],
       ),
     );
   }
--- lib/ui/welcome/welcome.dart
+++ lib/ui/welcome/welcome.dart
@@ -1,4 +1,8 @@
+import 'package:default_project/ui/app_routes.dart';
+import 'package:default_project/utils/svges.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 import '../../utils/colors.dart';
 
@@ -7,18 +11,34 @@ class WelcomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    _navigateToWelcomeScreen(context);
     return Scaffold(
+      backgroundColor: AppColors.C_121212,
       appBar: AppBar(
-        title: const Text(
-          "Welcome Screen",
-          style: TextStyle(color: AppColors.white),
-        ),
+        backgroundColor: AppColors.C_121212,
+        toolbarHeight: 0.h,
       ),
-      body: const Column(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text(""),
+          Center(
+            child: Column(
+              children: [
+                SvgPicture.asset(AppIcons.logo,height: 113.h,width: 113.w,),
+                SizedBox(height: 19.h,),
+                Text("UpTodo",style: TextStyle(fontWeight: FontWeight.w700,fontSize: 40.sp,fontFamily: "Lato",color: AppColors.white),),
+
+              ],
+            ),
+          )
+
         ],
       ),
     );
+  }
+  void _navigateToWelcomeScreen(BuildContext context) async{
+    Future.delayed(Duration(seconds: 3),(){
+      Navigator.pushReplacementNamed(context, RouteNames.splashScreen);
+    });
   }
 }
