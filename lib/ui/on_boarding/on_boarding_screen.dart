c2f4b5a287ee20fa87f131cbc197cbb2def14fb1
ParaDox-PDT <doniyorjorabekov@gmail.com>
2023-06-26 20:35:48 +0500

    update pageIndex logic

--- lib/ui/on_boarding/on_boarding_screen.dart
+++ lib/ui/on_boarding/on_boarding_screen.dart
@@ -1,14 +1,14 @@
 import 'package:default_project/ui/app_routes.dart';
+import 'package:default_project/ui/on_boarding/wdigets/page_view_index_img.dart';
 import 'package:default_project/ui/on_boarding/wdigets/pageview_items.dart';
 import 'package:default_project/utils/images.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:zoom_tap_animation/zoom_tap_animation.dart';
 
 import '../../utils/colors.dart';
-import '../../utils/svges.dart';
+
 
 class OnBoardingScreen extends StatefulWidget {
   const OnBoardingScreen({super.key});
@@ -65,7 +65,7 @@ class _OnBoardingScreenState extends State<OnBoardingScreen> {
 
                   },
                   controller: pageController,
-                  physics: const AlwaysScrollableScrollPhysics(),
+                  physics: const NeverScrollableScrollPhysics(),
                   scrollDirection: Axis.horizontal,
                   children: [
                     PageViewItems(title: tr("manage_your_tasks"), text: tr("on_boarding_text1"), img: AppImages.onBoarding1,),
@@ -73,31 +73,7 @@ class _OnBoardingScreenState extends State<OnBoardingScreen> {
                     PageViewItems(title: tr("organize_your_tasks"), text: tr("on_boarding_text3"),img: AppImages.onBoarding3,),
                   ],
                 ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    pageIndex == 0
-                        ? SvgPicture.asset(AppIcons.rec)
-                        : SvgPicture.asset(
-                            AppIcons.recPassive,
-                            color: Colors.white.withOpacity(0.2),
-                          ),
-                    SizedBox(width: 2.w),
-                    pageIndex == 1
-                        ? SvgPicture.asset(AppIcons.rec)
-                        : SvgPicture.asset(
-                            AppIcons.recPassive,
-                            color: Colors.white.withOpacity(0.2),
-                          ),
-                    SizedBox(width: 2.w),
-                    pageIndex == 2
-                        ? SvgPicture.asset(AppIcons.rec)
-                        : SvgPicture.asset(
-                            AppIcons.recPassive,
-                            color: Colors.white.withOpacity(0.2),
-                          ),
-                  ],
-                ),
+                PageIndexItem(activePageIndex: pageIndex),
               ],
             ),
           ),
